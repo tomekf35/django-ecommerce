37d533cab5625f31cf955e33a008f0dc9f19d98e
unknown <tomekf.dev@gmail.com>
2023-06-26 19:35:41 +0200

    accounts app started, CustomUser created

--- core/views.py
+++ core/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'core/index.html')
+
+def about_view(request):
+    return render(request, 'core/about.html')
+
+def contact_view(request):
+    return render(request, 'core/contact.html')
+
--- project_ecommerce/settings.py
+++ project_ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core.apps.CoreConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATICFILES_DIRS = ["static"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
