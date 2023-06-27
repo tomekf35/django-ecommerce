7312b0d1daf0c9de927194c7ab646f3e3ab04e4c
unknown <tomekf.dev@gmail.com>
2023-06-27 15:20:16 +0200

    items app started, models added

---
+++ items/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    stock = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f'{self.category} {self.name}'
+    
--- project_ecommerce/settings.py
+++ project_ecommerce/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     "core.apps.CoreConfig",
     "accounts.apps.AccountsConfig",
+    "items.apps.ItemsConfig",
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = ["static"]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
