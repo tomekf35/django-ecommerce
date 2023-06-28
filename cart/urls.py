b403cfbddc53950903044b8b70dd2dccca8d172b
unknown <tomekf.dev@gmail.com>
2023-06-28 15:41:13 +0200

    add to cart functionality added

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .import views
 
 
 app_name = 'cart'
 urlpatterns = [
-    
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('view/', views.view_cart, name='view_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from items.models import Product
+from .models import CartItem
 
-# Create your views here.
+
+@login_required
+def add_to_cart(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('cart:view_cart')
+
+
+@login_required
+def view_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
+
