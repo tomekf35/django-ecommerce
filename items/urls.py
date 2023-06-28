4ee5a9accae40566b881ba8e4bca026d8bb3f2ff
unknown <tomekf.dev@gmail.com>
2023-06-28 12:05:40 +0200

    Products categories added to views, urls, templates

--- items/urls.py
+++ items/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 app_name = 'items'
 urlpatterns = [
+    path('rig/', views.rig_products_view, name="products_rig"),
+    path('procesor/', views.procesor_products_view, name="products_procesor"),
+    path('gpu/', views.gpu_products_view, name="products_gpu"),
+    path('ram/', views.ram_products_view, name="products_ram"),
+    path('motherboard/', views.motherboard_products_view, name="products_motherboard"),
+    path('ssd/', views.ssd_products_view, name="products_ssd"),
+    path('psu/', views.psu_products_view, name="products_psu"),
+    path('tower/', views.tower_products_view, name="products_tower"),
     path('all/', views.all_products_view, name="products_all"),
 ]
--- items/views.py
+++ items/views.py
@@ -4,4 +4,51 @@ from .models import Product
 
 def all_products_view(request):
     products = Product.objects.all()
-    return render(request, 'items/item_list.html', {'products': products})
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'All Products'})
+    
+def rig_products_view(request):
+    products = Product.objects.filter(category__name='Rig')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'Rig'})
+    
+def procesor_products_view(request):
+    products = Product.objects.filter(category__name='Processor')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'Procesor'})
+    
+    
+def gpu_products_view(request):
+    products = Product.objects.filter(category__name='GPU')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'GPU Procesor'})
+    
+    
+def ram_products_view(request):
+    products = Product.objects.filter(category__name='RAM')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'RAM'})
+
+
+def motherboard_products_view(request):
+    products = Product.objects.filter(category__name='Motherboard')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'Motherboard'})
+    
+    
+def ssd_products_view(request):
+    products = Product.objects.filter(category__name='SSD')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'SSD'})
+    
+    
+def psu_products_view(request):
+    products = Product.objects.filter(category__name='PSU')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'PSU'})
+    
+    
+def tower_products_view(request):
+    products = Product.objects.filter(category__name='Tower')
+    return render(request, 'items/item_list.html', {'products': products, 
+                                                    'category': 'Tower'})
