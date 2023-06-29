8adf3925ea1c182578f583579dc056adafcc4929
unknown <tomekf.dev@gmail.com>
2023-06-29 11:28:27 +0200

    remove from cart added

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from .import views
 app_name = 'cart'
 urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('view/', views.view_cart, name='view_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum, F
 from items.models import Product
 from .models import CartItem
-
 
 @login_required
 def add_to_cart(request, product_id):
     product = Product.objects.get(pk=product_id)
     cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
 
-    if not created:
+    if created:
+        cart_item.quantity = 1
+    else:
         cart_item.quantity += 1
-        cart_item.save()
+    cart_item.save()
+
+    return redirect('cart:view_cart')
+
+
+@login_required
+def remove_from_cart(request, cart_item_id):
+    cart_item = CartItem.objects.get(pk=cart_item_id)
+    cart_item.delete()
 
     return redirect('cart:view_cart')
 
@@ -19,5 +29,7 @@ def add_to_cart(request, product_id):
 @login_required
 def view_cart(request):
     cart_items = CartItem.objects.filter(user=request.user)
-    return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
-
+    total_price = cart_items.aggregate(total_price=Sum(F('product__price') * F('quantity')))['total_price']
+    if total_price is None:
+        total_price = 0
+    return render(request, 'cart/view_cart.html', {'cart_items': cart_items, 'total_price': total_price})
